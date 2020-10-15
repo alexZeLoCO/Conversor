33e8b051d954a5ab2425219024db0795f3c58a7c
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-15 15:48:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+m = float (input ("Introduzca la distancia en metros: "))       #SOLICITA DATO
+
+pies = m * 3.2804       #CALCULA PIES
+yardas = m* 1.09361     #CALCULA YARDAS
+pulgadas = m * 39.3701      #CALCULA PULGADAS
+
+            #OUTPUT
+print (m,"metros son equivalentes a",pies,"pies",yardas,"yardas y a",pulgadas,"pulgadas.")
